82f6f9bf86b49057a1a0bd2eb33a779249858347
PrinceOfPuppers <joshuamcpherson5@gmail.com>
2021-11-06 02:02:58 -0400

    move ytInitalPage to its own file

--- degooged_tube/ytApiHacking/ytContIter.py
+++ degooged_tube/ytApiHacking/ytContIter.py
@@ -4,79 +4,11 @@ from typing import Union
 from dataclasses import dataclass
 
 from .jsonScraping import scrapeJsonTree, ScrapeNode
+from .ytInitalPage import YtInitalPage
 from . import controlPanel as ctrlp 
-from . import customExceptions as ce 
 
 import degooged_tube.config as cfg
 
-
-@dataclass
-class YtInitalPage:
-    url: str
-
-    key: str
-    clientVersion: str
-
-    continuations: dict
-
-    initalData: dict
-
-    @classmethod
-    def fromUrl(cls, url:str) -> 'YtInitalPage':
-        r=requests.get(url)
-
-        if r.status_code != 200:
-            raise requests.HTTPError(
-                f"Error Sending GET Request to: {url}\n"
-                f"Status: {r.status_code} {r.reason}"
-            )
-
-        x, y, z = ctrlp.apiKeyRe.search(r.text), ctrlp.ytInitalDataRe.search(r.text), ctrlp.clientVersionRe.search(r.text)
-
-        if not x:
-            raise ce.UnableToGetPage("Unable to Find INNERTUBE_API_KEY")
-
-        if not y:
-            raise ce.UnableToGetPage("Unable to Find Inital Data")
-
-        if not z:
-            raise ce.UnableToGetPage("Unable to Find Youtube Client Version")
-
-        key = x.group(1)
-        initalData = json.loads(y.group(1))
-        clientVersion = z.group(1)
-
-        a = scrapeJsonTree(initalData, ctrlp.continuationScrapeFmt)
-        assert type(a) is list
-        continuations = {}
-        for continuation in a:
-            try:
-                token = continuation['token']
-                apiUrl = continuation['apiUrl']
-            except KeyError:
-                continue
-
-            if apiUrl in continuations:
-                if token not in continuations[apiUrl]:
-                    continuations[apiUrl].append(token)
-                    cfg.logger.debug(f"Adding Additional Token to {apiUrl} of Page {url}\nNum Tokens Now: {len(continuations[apiUrl])}")
-            else:
-                continuations[apiUrl] = [token]
-
-        return cls(url, key, clientVersion, continuations, initalData )
-
-    def getContinuationTokens(self, apiUrl: str):
-        try:
-            return self.continuations[apiUrl].copy()
-        except KeyError:
-            raise KeyError(
-                f"apiUrl: {apiUrl} \n"
-                f"Does Not Match Any apiUrls: {self.continuations.keys()}\n"
-                f"In Inital Page: {self.url}\n"
-            )
-
-    def scrapeInitalData(self, dataFmt: ScrapeNode):
-        return scrapeJsonTree(self.initalData, dataFmt)
 
 
 
